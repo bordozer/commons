0a07fa4d8836acf613e556db1895340932e74c40
Borys Lukianov <blukianov@smartling.com>
2019-12-19 11:01:52 +0200

    v3.31 - no JSON in web logger

---
+++ src/main/java/com/bordozer/commons/batcher/package-info.java
@@ -0,0 +1,4 @@
+@NotNullByDefault
+package com.bordozer.commons.batcher;
+
+import com.bordozer.commons.annotations.NotNullByDefault;
--- src/main/java/com/bordozer/commons/web/WebLogger.java
+++ src/main/java/com/bordozer/commons/web/WebLogger.java
@@ -1,25 +1,26 @@
 package com.bordozer.commons.web;
 
-import com.bordozer.commons.utils.LoggableJson;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import javax.annotation.CheckForNull;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class WebLogger {
 
     public void logRequest(final RequestLogData requestLogData) {
-        final String headers = LoggableJson.of(requestLogData.getHttpHeaders().keySet().stream()
-                .collect(Collectors.toMap(head -> head, head -> requestLogData.getHttpHeaders().get(head)))).toString();
-        final String parameters = LoggableJson.of(requestLogData.getHttpParameters()).toString();
+        final String headers = extractHeader(requestLogData.getHttpHeaders());
+        final String parameters = extractParameters(requestLogData.getHttpParameters());
 
         log.info("Request: URI=\"{}\", method=\"{}\", contentType=\"{}\", headers=\"{}\", queryString=\"{}\", parameters=\"{}\", body=\"{}\"",
                 requestLogData.getUri(), requestLogData.getHttpMethod(), requestLogData.getContentType(), headers,
@@ -27,11 +28,30 @@ public class WebLogger {
     }
 
     public void logResponse(final ResponseLogData responseLogData) {
-        final String headers = LoggableJson.of(responseLogData.getHttpHeaders().keySet().stream().distinct()
-                .collect(Collectors.toMap(head -> head, head -> responseLogData.getHttpHeaders().get(head)))).toString();
+        final String headers = extractHeader(responseLogData.getHttpHeaders());
 
         log.info("Response: httpStatus=\"{}\", contentType=\"{}\", headers=\"{}\", body=\"{}\"",
                 responseLogData.getResponseStatus(), responseLogData.getContentType(), headers, responseLogData.getBody());
+    }
+
+    private static String extractHeader(final Map<String, String> httpHeaders) {
+        final Set<String> headerNames = httpHeaders.keySet();
+        return headerNames.stream()
+                .sorted()
+                .map(header -> String.format("%s=\"%s\"", header, httpHeaders.get(header)))
+                .collect(Collectors.joining(","));
+    }
+
+    private static String extractParameters(@CheckForNull final Map<String, String[]> parameterMap) {
+        if (parameterMap == null) {
+            return StringUtils.EMPTY;
+        }
+        return parameterMap.keySet().stream()
+                .map(param -> String.format("%s=[%s]", param, Arrays.stream(parameterMap.get(param))
+                        .map(pv -> String.format("\"%s\"", pv))
+                        .collect(Collectors.joining(",")))
+                )
+                .collect(Collectors.joining(","));
     }
 
     @Getter
